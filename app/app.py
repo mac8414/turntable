bbdb2ebe70eae3ef1005bbde0d5e77e4a18685b7
mac8414 <huntermk10@gmail.com>
2025-02-28 01:20:46 -0700

    last chage for the night

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import random
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -19,10 +19,16 @@ def home():
 
 @app.route('/random_album')
 def random_album():
-    # Generate a random search query
-    random_letter = random.choice("abcdefghijklmnopqrstuvwxyz")
-    results = spotify.search(q=random_letter, type="album", limit=1, offset=random.randint(0, 50))
-    
+    year = request.args.get('year')
+    genre = request.args.get('genre')
+    if year is not None and year != "Random Year":
+        query = f'year:{year}'
+        results = spotify.search(q=query, type="album", limit=1, offset=random.randint(0, 50))
+        
+    else:
+        random_letter = random.choice("abcdefghijklmnopqrstuvwxyz")
+        results = spotify.search(q=random_letter, type="album", limit=1, offset=random.randint(0, 50))
+
     # Get album details
     if results['albums']['items']:
         album = results['albums']['items'][0]
@@ -33,7 +39,6 @@ def random_album():
         artist_id = album['artists'][0]['id']
         artist_info = spotify.artist(artist_id)
         genres = artist_info['genres']
-
         return jsonify(album_name=album_name, album_url=album_url, album_image=album_image, artist_name=artist_name, genres=genres)
     else:
         return jsonify(album_name=None)
